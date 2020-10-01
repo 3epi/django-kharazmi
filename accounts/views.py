80ed2c9fdd6083a45cacc16f1c386fc47515f52d
sorush_saghari <tionog.hma@gmail.com>
2020-10-01 13:32:33 +0330

    request.method is a string not a function

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 def signup_view(request):
-    if request.method() == "POST":
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
