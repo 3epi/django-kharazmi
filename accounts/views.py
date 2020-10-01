8d8749563da8e951be8544267cfd292a17d28304
3epi <mohammadsepehr.bazargan@gmail.com>
2020-10-01 14:40:35 +0330

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render , redirect
-from django.contrib.auth.forms import UserCreationForm
-# Create your views here.
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login
+
 def signup_view(request):
-    if request.method() == "POST":
+    if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            # return redirect()
+        return redirect('urls:login')
     else : 
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form':form})
+def login_view(request):
+    if request.method== "POST" : 
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+        #return redirect('panel:panel')
+    else : 
+        form = AuthenticationForm()
+    return render (request, 'accounts/login.html', {'form':form})
