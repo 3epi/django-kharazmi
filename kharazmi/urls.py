cc193737d023539d6cb92c9b8f08b55ceca52166
3epi <mohammadsepehr.bazargan@gmail.com>
2020-10-01 13:02:37 +0330

    signup

--- kharazmi/urls.py
+++ kharazmi/urls.py
@@ -3,16 +3,19 @@ from django.urls import path
 from django.conf.urls import include
 from . import views 
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about),
     path('', views.home),
-    path('panel/', views.panel),
-    path('login/', views.login),
-    path('signup/', views.signup),
-    path('articles/', include('articles.urls'))
+    path('articles/', include('articles.urls')),
+    path('accounts/', include('accounts.urls')),
 
 
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL , document_root =settings.MEDIA_ROOT)
--- kharazmi/views.py
+++ kharazmi/views.py
@@ -5,9 +5,3 @@ def about(request):
 	return render(request , 'about.html')
 def home(request):
 	return render(request , 'home.html')
-def panel(request):
-	return render(request , 'panel.html')
-def login(request):
-	return render(request , 'login.html')
-def signup(request):
-	return render(request , 'signup.html')
