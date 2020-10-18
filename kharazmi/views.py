6b9d538529e084e3ef0a86bc52c3f93b3fd60285
3epi <mohammadsepehr.bazargan@gmail.com>
2020-10-18 20:09:44 +0330

    edits

--- kharazmi/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
-
-def about(request):
-	return render(request , 'about.html')
-def home(request):
-	return render(request , 'home.html')
